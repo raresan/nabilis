7078d68f074c932b63277c965e3504d04f9a81d5
Renan Alonso <renan.alonso91@gmail.com>
2024-09-18 01:23:18 -0300

    feat: menu initial structure

--- src/components/ui/Menu/Menu.tsx
+++ src/components/ui/Menu/Menu.tsx
@@ -1,15 +1,37 @@
-// import clsx from 'clsx'
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+import { Splash } from 'next/font/google'
 
 // import type { IMenu } from '@/components/ui/Menu/MenuTypes'
 
 import s from '@/components/ui/Menu/Menu.module.scss'
 
+const splash = Splash({ weight: '400', subsets: ['latin'] })
+
 const Menu = () => {
   return (
-    <header className={s.menu}>
-      <div>Logo</div>
+    <header className={s.header}>
+      <Link href='/'>
+        <h1 className={clsx(s.logo, splash.className)}>guik.tattoo</h1>
+      </Link>
 
-      <nav>Menu</nav>
+      <nav>
+        <ul className={clsx(s.menu)}>
+          <li className={clsx(s.menu__item)}>
+            <Link href='/'>Home</Link>
+          </li>
+          <li className={clsx(s.menu__item)}>
+            <Link href='/#about'>Sobre Mim</Link>
+          </li>
+          <li className={clsx(s.menu__item)}>
+            <Link href='/#gallery'>Tattoos</Link>
+          </li>
+          <li className={clsx(s.menu__item)}>
+            <Link href='/#contact'>Contato</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
